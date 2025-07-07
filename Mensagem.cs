f4894d30d64f1bdf896bbc60744f2af09fad73af
unknown <kaborges0203@gmail.com>
2025-07-06 23:56:17 -0300

    Adicionando arquivos do projeto completo

---
+++ Mensagem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace PooMensagem
+{
+    public class Mensagem
+    {
+        //private string TextoMensagem;
+
+        //// Método correto
+        //public void ExibirMensagem()
+        //{
+        //    Console.WriteLine(TextoMensagem);
+        //}
+
+        //public String getTextoMensagem()
+        //{
+        //    return this.TextoMensagem;
+        //}
+        //public void setTextoMensagem(String valor)
+        //{
+        //    this.TextoMensagem = valor.ToUpper();
+
+        //}
+
+        private String textomensagem;
+        public String Textomensagem
+        { 
+            get //representa método de acesso que vai pegar um valor da propriedade 
+            { return this.textomensagem; 
+            } 
+            set // define o valor da propriedade
+            { this.textomensagem = value.ToUpper(); 
+            } 
+        }
+
+        public void ExibirMensagem()
+        {
+           Console.WriteLine(this.Textomensagem);
+        }
+    }
+
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PooMensagem;
+
+Mensagem msg1, msg2;
+msg1 = new Mensagem();
+msg1.Textomensagem = "Bem vinda a programação!";
+msg1.ExibirMensagem();
+
+//msg2 = new Mensagem();
+//msg2.TextoMensagem = "Segundo objeto";
+//msg2.ExibirMensagem();
+
+Console.ReadKey();
+
+// todo objeto que eu criar vai possuir suas propias caracteristicas 
